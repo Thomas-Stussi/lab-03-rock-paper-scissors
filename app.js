b9f8462b4ca8a0f823633f7d97f7bb74c6a441cd
Thomas Stussi <tstussi06@gmail.com>
2020-07-08 15:03:06 -0700

    html mostly functional, grabbing DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
+const button = document.querySelector('#guess-button');
+const winLoseDraw = document.querySelector('#winlosedraw');
+const wins = document.querySelector('#wins');
+const losses = document.querySelector('#losses');
+const draws = document.querySelector('#draws');
 
 // initialize state
+let winsData = 0;
+let lossData = 0;
+let drawData = 0;
 
 // set event listeners to update state and DOM
