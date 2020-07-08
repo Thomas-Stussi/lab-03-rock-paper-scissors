91433801d40f3b504a84d2e3950d25b86d4e5a58
Thomas Stussi <tstussi06@gmail.com>
2020-07-08 16:33:54 -0700

    established player and computer variables

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-console */
+
+
 // import functions and grab DOM elements
+
 const button = document.querySelector('#guess-button');
 const winLoseDraw = document.querySelector('#winlosedraw');
 const wins = document.querySelector('#wins');
@@ -17,12 +21,46 @@ let drawData = 0;
 button.addEventListener ('click', () => {
     //get the value of the currently selected radio
     const checkedRadio = document.querySelector('input:checked');
-    const userGuess = checkedRadio.value;
+    const player = checkedRadio.value;
     //console
-    //console.log(userGuess);
+    //console.log(player);
 
     //create a randomizer for the computer
-    const random = Math.floor(Math.random() * 3);
-    //console.log(random);
+    const getRandomThrow = Math.floor(Math.random() * 3);
+    //console.log(getRandomThrow);
+
+    //create function that converts computerPlay into computerThrow
+    function computerThrow() {
+        if (getRandomThrow === 0) {
+            return 'rock';
+        }
+        if (getRandomThrow === 1) {
+            return 'paper';
+        }
+        if (getRandomThrow === 2) {
+            return 'scissors';
+        }
+    }
+    const computer = computerThrow();
+
+    console.log(computer);
+    console.log(player);
+
+    //add function with nine different win loss draw scenarios
+    //rock scenarios
+   
+    //paper scenarios
+
+    //scissors scenarios
+
+    //something
+
+
+    //console wins draws and losses
+    console.log(winsData);
+    console.log(lossData);
+    console.log(drawData);
+
+
 }
 );
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,30 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// import { example } from '../example.js'
+const computerPlay = Math.floor(Math.random() * 3);
+function computerThrow() {
+    if (computerPlay === 0) {
+        return 'rock';
+    }
+    if (computerPlay === 1) {
+        return 'paper';
+    }
+    if (computerPlay === 2) {
+        return 'scissors';
+    }
+}
+
+    
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should take in a number and return rock paper or scissors', function(assert) {
     //Arrange
-    // Set up your arguments and expectations
+    // Set up your parameters and expectations
     const expected = true;
-    
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
-
-    //Expect
+    const actual = computerThrow();
+    //Assert
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    assert.equal(expected, actual === 'rock' || actual === 'paper' || actual === 'scissors');
 });
