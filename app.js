8a7253620f6f5e6a714e39f286ccbc23e2312dc1
Thomas Stussi <tstussi06@gmail.com>
2020-07-08 15:11:49 -0700

    added event listener and initialized state

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const wins = document.querySelector('#wins');
 const losses = document.querySelector('#losses');
 const draws = document.querySelector('#draws');
 
+
+
 // initialize state
 let winsData = 0;
 let lossData = 0;
 let drawData = 0;
 
 // set event listeners to update state and DOM
+
+button.addEventListener ('click', () => {
+    //get the value of the currently selected radio
+    const checkedRadio = document.querySelector('input:checked');
+    const userGuess = checkedRadio.value;
+    //console
+    //console.log(userGuess);
+});
